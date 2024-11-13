0a3614452eff59fa3e7000b57e3f7c599a07ed2a
Ammar-khan04 <ammarkhan.ak04@gmail.com>
2024-11-14 00:31:01 +0500

    innit

---
+++ src/main/java/SCD-Project/App.java
@@ -0,0 +1,13 @@
+package SCD-Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
