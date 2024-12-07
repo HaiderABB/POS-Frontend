79f7f205a46a9ee1d4b4cc078994698fbb9ae24a
HaiderABB <haider.a.moazzam@gmail.com>
2024-12-07 18:14:48 +0500

    minor update

--- src/main/java/SCD/model/service/TestService.java
+++ src/main/java/SCD/model/service/TestService.java
@@ -2,10 +2,7 @@ package SCD.model.service;
 
 import SCD.model.models.Branch;
 import SCD.model.models.Employee;
-import SCD.model.service.BranchManagerService.BranchManagerService;
-import SCD.model.service.CashierService.CashierService;
 import SCD.model.service.Common.CommonServices;
-import SCD.model.service.DataEntryOperatorService.DataEntryOperatorService;
 import SCD.model.service.SuperAdminService.SuperAdminService;
 
 public class TestService {
@@ -14,19 +11,17 @@ public class TestService {
 
                 CommonServices commonServices = new CommonServices();
                 SuperAdminService superAdminService = new SuperAdminService();
-                BranchManagerService branchManagerService = new BranchManagerService();
-                DataEntryOperatorService dataEntryOperatorService = new DataEntryOperatorService();
-                CashierService cashierService = new CashierService();
+                ;
 
                 Branch branch = new Branch("BR-0001", "Main Branch", "Lahore", "Gulberg Main Boulevard", "03219306126");
 
-                superAdminService.createBranch(branch);
+                // superAdminService.createBranch(branch);
 
                 Employee employee = new Employee("SM-0002", "Haider Abbas Moazzam",
                                 "SUPER_ADMIN", branch, "03219306127", 20000,
                                 "haider.a.moazzam@gmail.com");
 
-                commonServices.AddEmployee(employee);
+                // commonServices.AddEmployee(employee);
 
         }
 
